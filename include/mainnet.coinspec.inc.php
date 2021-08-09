0686ee42133cd84a0b55149b68605c2fcb93bf68
phpcoinn <phpcoinn@gmail.com>
2021-08-09 18:39:23 +0200

    Cleanup

---
+++ include/mainnet.coinspec.inc.php
@@ -0,0 +1,53 @@
+<?php
+
+const VERSION = "1.0.0-alpha";
+const VERSION_CODE = "010000";
+const DEVELOPMENT = true;
+const XDEBUG = "XDEBUG_SESSION_START=PHPSTORM";
+const XDEBUG_CLI = "-dxdebug.mode=debug -dxdebug.client_host=127.0.0.1 -dxdebug.client_port=9000 -dxdebug.start_with_request=yes";
+
+const COIN = "phpcoin";
+const COIN_NAME="PHPCoin";
+const COIN_SYMBOL="PHP";
+const NETWORK_PREFIX = "38";
+const COIN_DECIMALS = 8;
+const BASE_REWARD = 100;
+const GENESIS_REWARD = 4900010;
+const BLOCKCHAIN_CHECKPOINT = 1;
+
+const BLOCK_TIME = 60;
+const BLOCK_TARGET_MUL = 1000;
+const BLOCK_START_DIFFICULTY = "60000";
+
+const TX_FEE = 0;
+const TX_TYPE_REWARD = 0;
+const TX_TYPE_SEND = 1;
+
+const HASHING_ALGO = PASSWORD_ARGON2I;
+const HASHING_OPTIONS = ['memory_cost' => 2048, "time_cost" => 1, "threads" => 1];
+const REMOTE_PEERS_LIST_URL = "http://spectre:8000/peers.php";
+
+const REWARD_SCHEME = [
+	'genesis' => [
+		'reward'=> GENESIS_REWARD
+	],
+	'launch'=>[
+		'blocks' => 10000,
+		'reward'=> 10,
+	],
+	'mining'=>[
+		'segments'=>10,
+		'block_per_segment'=>20000
+	],
+	'combined'=>[
+		'segments'=>10,
+		'block_per_segment'=>50000
+	],
+	'deflation'=>[
+		'segments'=>10,
+		'block_per_segment'=>100000
+	]
+];
+
+const TOTAL_SUPPLY = 210000000;
+const GIT_URL = "https://github.com/phpcoinn/node";
