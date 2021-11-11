3b8f9fd4a9309bced5c59d8666fc71bcea0f7fc6
phpcoinn <phpcoinn@gmail.com>
2021-11-11 20:37:25 +0100

    Regenerated genesis

--- include/genesis.inc.php
+++ include/genesis.inc.php
@@ -1,11 +1,11 @@
 <?php
 const GENESIS_DATA = [
-"signature" => 'AN1rKvtNLQBLW73NnyjR3GtaAo33fqRBEKN8x9HjWW95NSHw8HGBrybwqKxAnXFi7JnPuv4ZtQMVGgdhT4wwJXxVmodT74Lxs',
-"public_key" => 'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCzYF5yBLksha8hStWpsE5EJXcnLi16ZSbyNKBNQScbw6cYSf1wCwXCvbeZRH9twkGextift2R4sngugQdyqyJQt5A',
-"argon" => '$argon2i$v=19$m=2048,t=1,p=1$UHBaRmFvVmhSUzhuWnpXUA$C2qeZmJLAmgm/OsXwXovVRCOlVBunCwT8S51RsyXzkc',
+"signature" => 'AN1rKvtK3T1BTx9ptY3i7XfKhRXNi4mBXdrqX6gzr4984xnnxNoPnFnaXzsMEesA25H2ULPnXMru5R98okTpB7Ux83tsuTTUj',
+"public_key" => 'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCzn7YbwQ4KfEfp3QLZJvQuxgfxEbopcGyVhiZr666X352TaGXEGAX1PcmtXCb7rWvmBxRMVC3yicp1GcgBFzF7tF2',
+"argon" => '$argon2i$v=19$m=2048,t=1,p=1$UGNCREo1SkpKenlLZUJndg$ROYdz1qBmyYuImfUlg9/CyOxalrVUZ+6bpxa1swL8zw',
 "difficulty" => '60000',
-"nonce" => '3898e0043ed4b5597dc9df5d5f9b859dbd3be74ffdb6d740c3ff0907e7c64c3e',
-"date" => '1636647747',
-"reward_tx" => '{"1ieHsT6AWWBtLXkawxNNwNCQXW4qKbvWqz9iJF8gShX":{"date":1636647747,"dst":"PpZFaoVhRS8nZzWPCKTZQf1B4HZqmUXCAi","fee":"0.00000000","id":"1ieHsT6AWWBtLXkawxNNwNCQXW4qKbvWqz9iJF8gShX","message":"This is genesis","public_key":"PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCzYF5yBLksha8hStWpsE5EJXcnLi16ZSbyNKBNQScbw6cYSf1wCwXCvbeZRH9twkGextift2R4sngugQdyqyJQt5A","signature":"iKx1CJNKTrD24RAKnG7CNEeHNEDkbmXL3SSsvg6APp2wzmsS6sQ7Ex7x9LThTQPPwqPVePzxSSm7k3MkpgbVzEnjT4ugUAB7RC","src":"PpZFaoVhRS8nZzWPCKTZQf1B4HZqmUXCAi","type":0,"val":"4900010.00000000"}}',
+"nonce" => '906634de25024432d1af84a348f434f3b9fcf31a72c8b6d3df96f12b01aedf57',
+"date" => '1636659426',
+"reward_tx" => '{"FwTpRshuzepsZsNcQSwfPouDoRrWYZWEcTdPDxuqe6tc":{"date":1636659426,"dst":"PcBDJ5JJJzyKeBgvKLpYynEctG5LnkuNsj","fee":"0.00000000","id":"FwTpRshuzepsZsNcQSwfPouDoRrWYZWEcTdPDxuqe6tc","message":"This is genesis","public_key":"PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCzn7YbwQ4KfEfp3QLZJvQuxgfxEbopcGyVhiZr666X352TaGXEGAX1PcmtXCb7rWvmBxRMVC3yicp1GcgBFzF7tF2","signature":"AN1rKvtiAdnvUax7pJggqWyUoudhgiCJteepJimMPCwQEzVpC1VHC36bJdNcSWAF3yYaerqSJi3BvwYiqJtqz8FkGv4BKDssg","src":"PcBDJ5JJJzyKeBgvKLpYynEctG5LnkuNsj","type":0,"val":"4900010.00000000"}}',
 ];
-const GENESIS_TIME = 1636647747;
+const GENESIS_TIME = 1636659426;
