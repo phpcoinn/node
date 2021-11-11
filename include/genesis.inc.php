216f8dadb7c6e71c16a72d36895f8b9c7a987620
phpcoinn <phpcoinn@gmail.com>
2021-11-11 17:42:52 +0100

    Added genesis data

---
+++ include/genesis.inc.php
@@ -0,0 +1,11 @@
+<?php
+const GENESIS_DATA = [
+"signature" => 'AN1rKvtNLQBLW73NnyjR3GtaAo33fqRBEKN8x9HjWW95NSHw8HGBrybwqKxAnXFi7JnPuv4ZtQMVGgdhT4wwJXxVmodT74Lxs',
+"public_key" => 'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCzYF5yBLksha8hStWpsE5EJXcnLi16ZSbyNKBNQScbw6cYSf1wCwXCvbeZRH9twkGextift2R4sngugQdyqyJQt5A',
+"argon" => '$argon2i$v=19$m=2048,t=1,p=1$UHBaRmFvVmhSUzhuWnpXUA$C2qeZmJLAmgm/OsXwXovVRCOlVBunCwT8S51RsyXzkc',
+"difficulty" => '60000',
+"nonce" => '3898e0043ed4b5597dc9df5d5f9b859dbd3be74ffdb6d740c3ff0907e7c64c3e',
+"date" => '1636647747',
+"reward_tx" => '{"1ieHsT6AWWBtLXkawxNNwNCQXW4qKbvWqz9iJF8gShX":{"date":1636647747,"dst":"PpZFaoVhRS8nZzWPCKTZQf1B4HZqmUXCAi","fee":"0.00000000","id":"1ieHsT6AWWBtLXkawxNNwNCQXW4qKbvWqz9iJF8gShX","message":"This is genesis","public_key":"PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCzYF5yBLksha8hStWpsE5EJXcnLi16ZSbyNKBNQScbw6cYSf1wCwXCvbeZRH9twkGextift2R4sngugQdyqyJQt5A","signature":"iKx1CJNKTrD24RAKnG7CNEeHNEDkbmXL3SSsvg6APp2wzmsS6sQ7Ex7x9LThTQPPwqPVePzxSSm7k3MkpgbVzEnjT4ugUAB7RC","src":"PpZFaoVhRS8nZzWPCKTZQf1B4HZqmUXCAi","type":0,"val":"4900010.00000000"}}',
+];
+const GENESIS_TIME = 1636647747;
