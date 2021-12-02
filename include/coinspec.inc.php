68dad6b7c7180fc54e8ad1d03addfd072c735d4d
phpcoinn <86735260+phpcoinn@users.noreply.github.com>
2021-12-02 09:57:36 +0100

    Update coinspec.inc.php
    Reverted version

--- include/coinspec.inc.php
+++ include/coinspec.inc.php
@@ -5,7 +5,7 @@ if(file_exists(dirname(__DIR__)."/testnet")) {
 }
 
 // mainnet specification
-const VERSION = "1.0.2";
+const VERSION = "1.0.1";
 const BUILD_VERSION = 12;
 const DEVELOPMENT = false;
 const XDEBUG = "";
