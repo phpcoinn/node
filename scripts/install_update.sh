06197a4678ab35ade18a40dd43b0fbea48bbe5df
phpcoinn <phpcoinn@gmail.com>
2021-12-08 17:44:23 +0100

    Install update script

--- scripts/install_update.sh
+++ scripts/install_update.sh
@@ -2,12 +2,13 @@
 
 # curl -s https://raw.githubusercontent.com/phpcoinn/node/main/scripts/install_update.sh | bash
 
-CRON_EXISTS=$(crontab -l | grep '/var/www/phpcoin/scripts/update.sh' | wc -l)
+CRON_LINE="cd /var/www/phpcoin && php cli/util.php update"
+CRON_EXISTS=$(crontab -l | grep "$CRON_LINE" | wc -l)
 
 if [ $CRON_EXISTS -eq 0 ]
 then
 	chmod +x /var/www/phpcoin/scripts/update.sh
-	crontab -l | { cat; echo "*/5 * * * * /var/www/phpcoin/scripts/update.sh"; } | crontab -
+	crontab -l | { cat; echo "*/5 * * * * $CRON_LINE"; } | crontab -
 	echo "Added new cron line"
 else
 	echo "Cron entry exists"
