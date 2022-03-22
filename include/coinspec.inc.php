b352b2c5fca029c936629ed36bc31f2be6c52c9f
phpcoinn <phpcoinn@gmail.com>
2022-03-22 20:54:26 +0100

    Fixed peering masternodes

--- include/coinspec.inc.php
+++ include/coinspec.inc.php
@@ -7,7 +7,7 @@ if(file_exists(dirname(__DIR__)."/testnet")) {
 // mainnet specification
 const NETWORK = "mainnet-alpha";
 const VERSION = "1.0.3";
-const BUILD_VERSION = 55;
+const BUILD_VERSION = 56;
 const MIN_VERSION = "1.0.2";
 const DEVELOPMENT = false;
 const XDEBUG = "";
