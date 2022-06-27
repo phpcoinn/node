3ecd7260fab4777a29460f2ae7014131e8c7087f
phpcoinn <phpcoinn@gmail.com>
2022-06-27 19:08:09 +0200

    Pushed new version

--- include/coinspec.inc.php
+++ include/coinspec.inc.php
@@ -7,7 +7,7 @@ if(file_exists(dirname(__DIR__)."/testnet")) {
 // mainnet specification
 const NETWORK = "mainnet-alpha";
 const VERSION = "1.0.5";
-const BUILD_VERSION = 75;
+const BUILD_VERSION = 76;
 const MIN_VERSION = "1.0.4";
 const DEVELOPMENT = false;
 const XDEBUG = "";
