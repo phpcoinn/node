29ae45a31f0810be6230cc7681418f05619dfbb6
phpcoinn <phpcoinn@gmail.com>
2022-07-22 20:14:18 +0200

    Fixed db class to allow "show" query New version push

--- include/db.inc.php
+++ include/db.inc.php
@@ -110,7 +110,7 @@ class DB extends PDO
         try {
             $pdostmt = $this->prepare($this->sql);
             if ($pdostmt->execute($this->bind) !== false) {
-                if (preg_match("/^(".implode("|", ["select", "describe", "pragma"]).") /i", $this->sql)) {
+                if (preg_match("/^(".implode("|", ["select", "describe", "pragma", "show"]).") /i", $this->sql)) {
 	                //$time2 = microtime(true);
 	                //$diff = round(($time2-$time1)*1000);
 	                //if($diff > 1000) {
