95a27c59706e86cf1deb6155c38cbbcd17915b87
phpcoinn <phpcoinn@gmail.com>
2022-08-19 22:19:22 +0200

    Test P2P propagation

--- include/coinspec.inc.php
+++ include/coinspec.inc.php
@@ -7,7 +7,7 @@ if(@file_exists(dirname(__DIR__)."/testnet")) {
 // mainnet specification
 const NETWORK = "mainnet-alpha";
 const VERSION = "1.0.6";
-const BUILD_VERSION = 95;
+const BUILD_VERSION = 96;
 const MIN_VERSION = "1.0.5";
 const DEVELOPMENT = false;
 const XDEBUG = "";
