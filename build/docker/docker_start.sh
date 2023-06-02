503b063107e8696cb59d3a879c8b55fd744dc53d
phpcoinn <phpcoinn@gmail.com>
2023-06-02 12:18:59 +0200

    Updated docker build

--- build/docker/docker_start.sh
+++ build/docker/docker_start.sh
@@ -16,23 +16,33 @@ if test -f "$FILE"; then
     echo "First run node"
     export IP=$(curl -s http://whatismyip.akamai.com/)
     echo "Node external IP=$IP"
+    echo "Node external port=$EXT_PORT"
     curl "http://127.0.0.1" > /dev/null 2>&1
     sleep 5
 
-    mysql $DB_NAME -e "update config set val='http://$IP' where cfg='hostname'"
+    mysql $DB_NAME -e "update config set val='http://$IP:$EXT_PORT' where cfg='hostname'"
 
-    echo "Import blockchain... "
+    echo "Import $NETWORK blockchain..."
 
     cd $NODE_DIR/tmp
-    wget -q https://phpcoin.net/download/blockchain.sql.zip
-    unzip blockchain.sql.zip > /dev/null 2>&1
+    if [ "$NETWORK" = "test" ]
+    then
+      wget https://phpcoin.net/download/blockchain-testnet.sql.zip -O blockchain.sql.zip
+    else
+      wget https://phpcoin.net/download/blockchain.sql.zip -O blockchain.sql.zip
+    fi
+
+    unzip blockchain.sql.zip
+    if [ "$NETWORK" = "test" ]
+    then
+      mv blockchain-testnet.sql blockchain.sql
+    fi
     cd $NODE_DIR
-    php cli/util.php importdb tmp/blockchain.sql > /dev/null
+    php cli/util.php importdb tmp/blockchain.sql
     rm first-run
     > tmp/phpcoin.log
 fi
 
 rm -rf /var/www/phpcoin/tmp/sync-lock
-
 echo "Ready"
 tail -f tmp/phpcoin.log
