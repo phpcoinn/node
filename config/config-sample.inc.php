41ebb74c4047f1eee6cba6435925cf4dffb00717
phpcoinn <phpcoinn@gmail.com>
2023-08-31 21:15:49 +0200

    Fixed config sample file

--- config/config-sample.inc.php
+++ config/config-sample.inc.php
@@ -7,7 +7,7 @@
 */
 
 $_config['chain_id'] = trim(file_get_contents(dirname(__DIR__)."/chain_id"));
-if($_config['chain_id'] != DEFAULT_CHAIN_ID && file_exists($_config['chain_id'].".inc.php")) {
+if($_config['chain_id'] != DEFAULT_CHAIN_ID && file_exists(__DIR__ . "/config." . $_config['chain_id'].".inc.php")) {
     require_once __DIR__ . "/config.".$_config['chain_id'].".inc.php";
     return;
 }
