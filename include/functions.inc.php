c05128f14ea9d87a4e187b9f85a48c83f37e051a
phpcoinn <phpcoinn@gmail.com>
2023-10-25 15:01:02 +0200

    Fixed catching error

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -392,7 +392,7 @@ function hashimg($hash, $title=null) {
 function try_catch($callback, &$error = null) {
 	try {
 		return call_user_func($callback);
-	} catch (Exception $e) {
+	} catch (Throwable $e) {
 		$stack = debug_backtrace();
 		$error = $stack[1]['class'].$stack[1]['type'].$stack[1]['function'].":".$stack[1]['line'].": " . $e->getMessage();
 		_log($error);
