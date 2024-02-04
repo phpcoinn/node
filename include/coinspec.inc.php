a4957f514044d586f644fce3a3472d93f35d8d0f
phpcoinn <phpcoinn@gmail.com>
2024-02-04 23:19:55 +0100

    Force delete blocks to specified height

--- include/coinspec.inc.php
+++ include/coinspec.inc.php
@@ -9,7 +9,7 @@ const NETWORK = "testnet";
 const CHAIN_ID = "01";
 const COIN_PORT = "";
 const VERSION = "1.3.8";
-const BUILD_VERSION = 406;
+const BUILD_VERSION = 407;
 const MIN_VERSION = "1.3.6";
 const DEVELOPMENT = false;
 const XDEBUG = "";
@@ -85,7 +85,7 @@ const FEATURE_APPS = false;
 const MAIN_DAPPS_ID = "PeC85pqFgRxmevonG6diUwT4AfF7YUPSm3";
 const TOTAL_INITIAL_SUPPLY = 103200000;
 
-const STOP_CHAIN_HEIGHT = 1042215;
+const STOP_CHAIN_HEIGHT = 1042199;
 const DELETE_CHAIN_HEIGHT = 1042199;
 
 const MN_CREATE_IGNORE_HEIGHT = [];
