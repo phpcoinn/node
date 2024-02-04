be2f4fffe7d314979df38d44b14eb724cb3b05af
phpcoinn <phpcoinn@gmail.com>
2024-02-05 00:47:31 +0100

    Push new version - resume blockchain

--- include/coinspec.inc.php
+++ include/coinspec.inc.php
@@ -9,7 +9,7 @@ const NETWORK = "testnet";
 const CHAIN_ID = "01";
 const COIN_PORT = "";
 const VERSION = "1.3.8";
-const BUILD_VERSION = 409;
+const BUILD_VERSION = 410;
 const MIN_VERSION = "1.3.6";
 const DEVELOPMENT = false;
 const XDEBUG = "";
@@ -85,8 +85,8 @@ const FEATURE_APPS = false;
 const MAIN_DAPPS_ID = "PeC85pqFgRxmevonG6diUwT4AfF7YUPSm3";
 const TOTAL_INITIAL_SUPPLY = 103200000;
 
-const STOP_CHAIN_HEIGHT = 1042199;
-const DELETE_CHAIN_HEIGHT = 1042199;
+const STOP_CHAIN_HEIGHT = 1042300;
+const DELETE_CHAIN_HEIGHT = PHP_INT_MAX;
 
 const MN_CREATE_IGNORE_HEIGHT = [];
 const MN_COLD_START_HEIGHT = 879000;
