2222208cda30f0de465f9600a3f7183b762d99a3
phpcoinn <phpcoinn@gmail.com>
2024-07-05 19:00:58 +0200

    Push new version - block old version

--- include/coinspec.inc.php
+++ include/coinspec.inc.php
@@ -8,9 +8,9 @@ if($_config['chain_id'] != DEFAULT_CHAIN_ID && file_exists(__DIR__ . "/coinspec.
 const NETWORK = "testnet";
 const CHAIN_ID = "01";
 const COIN_PORT = "";
-const VERSION = "1.4.9";
-const BUILD_VERSION = 458;
-const MIN_VERSION = "1.4.3";
+const VERSION = "1.5.0";
+const BUILD_VERSION = 459;
+const MIN_VERSION = "1.4.9";
 const GIT_BRANCH = "test";
 const MIN_MINER_VERSION = "1.3";
 
