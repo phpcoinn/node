d935f2f6d865838d5a93e76752cb746a3ab09c6d
phpcoinn <phpcoinn@gmail.com>
2024-08-17 13:16:26 +0200

    Clean state for chained contract

--- include/class/sc/SmartContractWrapper.php
+++ include/class/sc/SmartContractWrapper.php
@@ -64,7 +64,8 @@ class SmartContractWrapper
             $args['address']=$this->address;
             $this->log("args=".json_encode($args));
             $this->smartContract->setFields($args);
-            $this->loadState(true);
+            $this->cleanState($args['height']);
+            $this->loadState();
             $reflect = new ReflectionClass($this->smartContract);
             $method = $reflect->getMethod($methodName);
             $this->invoke($method, $params);
@@ -75,10 +76,10 @@ class SmartContractWrapper
         }
     }
 
-    private function loadState($ext=false) {
+    private function loadState() {
         $reflect = new ReflectionClass($this->smartContract);
         $props = $reflect->getProperties(ReflectionProperty::IS_PUBLIC);
-        $this->state = $this->readVarsState($ext);
+        $this->state = $this->readVarsState();
         foreach($props as $prop) {
             if($this->hasAnnotation($prop, "SmartContractVar")) {
                 $name = $prop->getName();
@@ -87,7 +88,7 @@ class SmartContractWrapper
         }
     }
 
-    private function readVarsState($ext=false) {
+    private function readVarsState() {
         $virtual=$this->args['virtual'];
         if($virtual) {
             $state = [];
@@ -107,7 +108,7 @@ class SmartContractWrapper
                   and s.var_key is null ) as ranked
                 where ranked.rn = 1
             ";
-            $rows = $this->db->run($sql, [":address"=> $this->address, ":height"=>$ext ? $height-1 : $height]);
+            $rows = $this->db->run($sql, [":address"=> $this->address, ":height"=>$height]);
             foreach ($rows as $row) {
                 $state[$row['variable']]=$row['var_value'];
             }
