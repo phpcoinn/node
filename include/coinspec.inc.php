4d4709631b5d7b996090445b5ecd18a4186f4b94
phpcoinn <phpcoinn@gmail.com>
2025-05-27 13:55:14 +0200

    New version push - test update

--- include/coinspec.inc.php
+++ include/coinspec.inc.php
@@ -10,7 +10,7 @@ const NETWORK = "testnet";
 const CHAIN_ID = "01";
 const COIN_PORT = "";
 const VERSION = "1.6.1";
-const BUILD_VERSION = 490;
+const BUILD_VERSION = 491;
 const MIN_VERSION = "1.5.8";
 const DEVELOPMENT = false;
 const XDEBUG = "&XDEBUG_SESSION_START=PHPSTORM";
