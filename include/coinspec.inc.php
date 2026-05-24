e198d1b60873d439a6afd35132b3a868a52c3beb
phpcoinn <phpcoinn@gmail.com>
2026-05-24 14:03:20 +0200

    Push new version 1.8.0.514

--- include/coinspec.inc.php
+++ include/coinspec.inc.php
@@ -9,8 +9,8 @@ if($_config['chain_id'] != DEFAULT_CHAIN_ID && file_exists(__DIR__ . "/coinspec.
 const NETWORK = "testnet";
 const CHAIN_ID = "01";
 const COIN_PORT = "";
-const VERSION = "1.7.1";
-const BUILD_VERSION = 513;
+const VERSION = "1.8.0";
+const BUILD_VERSION = 514;
 const MIN_VERSION = "1.7.0";
 const DEVELOPMENT = false;
 const XDEBUG = "&XDEBUG_SESSION_START=PHPSTORM";
