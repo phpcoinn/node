db54fa2b9eff9b70617ca57df1a6196c7f1d2133
phpcoinn <phpcoinn@gmail.com>
2026-06-01 12:31:27 +0200

    Synced crypto lib

---
+++ web/apps/common/js/phpcoin-crypto.esm.js
@@ -0,0 +1,18 @@
+import {sign as $2YgEg$sign, getPublicKey as $2YgEg$getPublicKey, getAddress as $2YgEg$getAddress, generateAccount as $2YgEg$generateAccount, encryptString as $2YgEg$encryptString, decryptString as $2YgEg$decryptString, importPrivateKey as $2YgEg$importPrivateKey, generateRandomString as $2YgEg$generateRandomString, sha256 as $2YgEg$sha256, verifyAddress as $2YgEg$verifyAddress} from "phpcoin-crypto";
+
+
+if (typeof window !== "undefined") {
+    window.sign = $2YgEg$sign;
+    window.get_public_key = $2YgEg$getPublicKey;
+    window.get_address = $2YgEg$getAddress;
+    window.generateAccount = $2YgEg$generateAccount;
+    window.encryptString = $2YgEg$encryptString;
+    window.decryptString = $2YgEg$decryptString;
+    window.importPrivateKey = $2YgEg$importPrivateKey;
+    window.getPublicKey = $2YgEg$getPublicKey;
+    window.generateRandomString = $2YgEg$generateRandomString;
+    window.sha256 = $2YgEg$sha256;
+    window.verifyAddress = $2YgEg$verifyAddress;
+}
+
+
