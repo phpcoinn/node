a96bf002a859c49212dc8a4510679d7290183e86
phpcoinn <phpcoinn@gmail.com>
2026-07-11 19:04:33 +0200

    Push new version for test 1.10.2.522

--- include/coinspec.01.inc.php
+++ include/coinspec.01.inc.php
@@ -4,8 +4,8 @@
 const NETWORK = "testnet";
 if(!defined('CHAIN_ID')) define('CHAIN_ID', "01");
 const COIN_PORT = "";
-const VERSION = "1.10.1";
-const BUILD_VERSION = 521;
+const VERSION = "1.10.2";
+const BUILD_VERSION = 522;
 const MIN_VERSION = "1.9.2";
 const DEVELOPMENT = false;
 const XDEBUG = "&XDEBUG_SESSION_START=PHPSTORM";
