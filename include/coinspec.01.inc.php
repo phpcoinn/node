449d817bb73abf0b8719385a7fb5b07dfdf5fc37
phpcoinn <phpcoinn@gmail.com>
2026-09-16 14:34:25 +0200

    testnet 1.13.2.534 -  testing dex

--- include/coinspec.01.inc.php
+++ include/coinspec.01.inc.php
@@ -4,8 +4,8 @@
 const NETWORK = "testnet";
 if(!defined('CHAIN_ID')) define('CHAIN_ID', "01");
 const COIN_PORT = "";
-const VERSION = "1.13.1";
-const BUILD_VERSION = 533;
+const VERSION = "1.13.2";
+const BUILD_VERSION = 534;
 const MIN_VERSION = "1.13.0";
 const DEVELOPMENT = false;
 const XDEBUG = "&XDEBUG_SESSION_START=PHPSTORM";
